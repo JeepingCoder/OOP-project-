f03550cd6cf762473e94539d5d84bfc288237128
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-03-12 17:58:02 -0400

    test code

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,3 +4,9 @@ test("Can instantiate Manager instance", () => {
     const manager = new Manager();
     expect(typeof(manager)).toBe("object");
 })
+
+test("Can get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new Manager("Foo", "test@test.com", 2, " ",  testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+  });
